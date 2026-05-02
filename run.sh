a73632555e3b2cdfbf0dd18892837e9668880d92
Alex Hunt <alex.s.hunt7@gmail.com>
2026-05-02 14:00:28 +0100

    Set workdir to the same path inside container as outside

--- run.sh
+++ run.sh
@@ -2,9 +2,11 @@
 set -exuo pipefail
 
 script_dir="$(dirname "$(realpath "$0")")"
+workdir="$(pwd)"
 
 podman run -it --rm \
     --net host \
     -v "${script_dir}/agent:/root/.pi/agent" \
-    -v "$(pwd):/workspace" \
+    -v "${workdir}:${workdir}" \
+    --workdir "${workdir}" \
     pi-sandbox "$@"
